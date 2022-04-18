d6a8f0576193d2c9992df705397c5b4938a23b53
Jarno02 <jarno.ryhanen02@gmail.com>
2022-04-18 13:59:15 +0300

    -Deleted RecommendationsFragment

--- app/src/main/java/com/home/libraryapp/features/recommendations/RecommendationsFragment.kt
+++
@@ -1,9 +0,0 @@
-package com.home.libraryapp.features.recommendations
-
-import androidx.fragment.app.Fragment
-import com.home.libraryapp.R
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class RecommendationsFragment : Fragment(R.layout.fragment_books_recommendations) {
-}
